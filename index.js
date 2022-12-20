5cc47e52b13748a0e90f2212c86b7f39cc78c1dd
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2022-12-20 20:54:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.use("/openai", require("./routes/openaiRoutes"));
+
+app.listen(port, () => console.log(`Server is running at port ${port}`));
